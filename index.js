9f3dc4416b2e60af74763f1b127f62236df5fba1
Brian Pfeil <brian.pfeil@gmail.com>
2020-10-26 12:48:33 -0400

    added middlewareLogger

--- index.js
+++ index.js
@@ -4,10 +4,23 @@ const listTables = async () => {
   try {
     const dynamodb = new DynamoDB();
 
+    const middlewareLogger = (step) => {
+      return (next, context) => (args) => {
+        console.log(`--step: ${step}--\n`, args);
+        return next(args);
+      };
+    };
+
+    // log every step
+    "initialize,serialize,build,finalizeRequest,deserialize"
+      .split(",")
+      .forEach((step) => {
+        dynamodb.middlewareStack.add(middlewareLogger(step), { step });
+      });
+
     // example using middleware to add custom header
     dynamodb.middlewareStack.add(
       (next, context) => (args) => {
-        console.log(args);
         args.request.headers["Custom-Header"] = "value";
         return next(args);
       },
